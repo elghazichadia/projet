40f4ba62517080245eaf3d664c27d290715d9bbd
marouanetaki <34253135+marouanetaki@users.noreply.github.com>
2019-05-09 14:36:28 +0000

    Update main.js

--- fichier/js/main.js
+++ fichier/js/main.js
@@ -88,10 +88,12 @@ jQuery(document).ready(function($){
 
 	function addProduct() {
 		//this is just a product placeholder
+		productname = document.getElementsByClassName('media-heading').value;
+		productprice = document.getElementsByClassName('mu-menu-price').value;
 		//you should insert an item with the selected product info
 		//replace productId, productName, price and url with your real product info
 		productId = productId + 1; 
-		var productAdded = $('<li class="product"><div class="product-image"><a href="#0"><img src="img/product-preview.png" alt="placeholder"></a></div><div class="product-details"><h3><a href="#0">Product Name</a></h3><span class="price">$25.99</span><div class="actions"><a href="#0" class="delete-item">Delete</a><div class="quantity"><label for="cd-product-'+ productId +'">Qty</label><span class="select"><select id="cd-product-'+ productId +'" name="quantity"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option></select></span></div></div></div></li>');
+		var productAdded = $('<li class="product"><div class="product-image"><a href="#0"><img src="img/burger4.jpg" alt="placeholder"></a></div><div class="product-details"><h3><a href="#0">productname</a></h3><span class="price">productprice</span><div class="actions"><a href="#0" class="delete-item">Delete</a><div class="quantity"><label for="cd-product-'+ productId +'">Qty</label><span class="select"><select id="cd-product-'+ productId +'" name="quantity"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option></select></span></div></div></div></li>');
 		cartList.prepend(productAdded);
 	}
 
