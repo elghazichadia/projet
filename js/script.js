e36c8c29e61a886c9930b67b68b7b7b2e11ff878
AmalAl1 <48999819+AmalAl1@users.noreply.github.com>
2019-05-12 17:26:41 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,10 +11,10 @@ $(document).ready(function() {
 });
 
 function validateCheckoutform() {
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+
   var forms = document.getElementsByClassName("needs-validation");
 
-  // Loop over them and prevent submission
+ 
   var validation = Array.prototype.filter.call(forms, function(form) {
     form.addEventListener(
       "submit",
@@ -37,7 +37,7 @@ function attachTopScroller (elementId){
           $(elementId).fadeOut();
       }
   });
-  // Scroll To Top Animation
+
   $(elementId).click(function() {
       $("html, body").animate({
           scrollTop: 0
